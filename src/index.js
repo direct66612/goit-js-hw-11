56a18af14d1e51608bf78b384ab5140d73017de2
direct66612 <direct66612@gmail.com>
2023-10-12 01:22:41 -0400

    update

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@
 //   console.log('!!!');
 //   alert('!!!!');
 // }
-import axios from './axios';
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_Oiryl9hZcjoWsL43Z4gHDUXT7S11BeSgku8RiLsfFqqOJC3kmsWhLuScznWCgdA3';
+
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
+function fetchBreeds() {
+  return axios.get('breeds').then(res => res.data);
+}
+function fetchCatByBreed(breedId) {
+  return axios.get(`images/search?breed_ids=${breedId}`).then(res => res.data);
+}
+fetchBreeds().then(data => console.log(data));
